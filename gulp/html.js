fa5e4fe967214770e2d9747966d896a13b0ea1c3
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2016-08-10 15:02:10 +0100

    test commit. show me a fail

--- gulp/html.js
+++ gulp/html.js
@@ -65,7 +65,7 @@ function htmllintReporter(filepath, issues) {
   if (issues.length > 0) {
 
       issues.forEach(function (issue) {
-          gutil.log(gutil.colors.cyan('[gulp-htmllint] ') + gutil.colors.white(filepath + ' [' + issue.line + ',' + issue.column + ']: ') + gutil.colors.red('(' + issue.code + ') ' + issue.msg));
+          gutil.log(gutil.colors.red('[gulp-htmllint] ') + gutil.colors.white(filepath + ' [' + issue.line + ',' + issue.column + ']: ') + gutil.colors.red('(' + issue.code + ') ' + issue.msg));
       });
 
       process.exitCode = 1;
